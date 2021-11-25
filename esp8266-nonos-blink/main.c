2a5eceb766d00540f093198648a2c4e631188020
SC <hocarm.org@gmail.com>
2021-11-25 22:07:11 +0700

    Update with new compiler crosstool-NG esp-2020r3-49-gd5524c1 and clean up files

--- esp8266-nonos-blink/main.c
+++ esp8266-nonos-blink/main.c
@@ -14,10 +14,10 @@ void led_service_cb(void *args)
 void app_init()
 {
     uart_div_modify(0, UART_CLK_FREQ / 115200);
-    WRITE_PERI_REG(PAD_XPD_DCDC_CONF, (READ_PERI_REG(PAD_XPD_DCDC_CONF) & 0xffffffbc) | (uint32_t)0x1);  
+    WRITE_PERI_REG(PAD_XPD_DCDC_CONF, (READ_PERI_REG(PAD_XPD_DCDC_CONF) & 0xffffffbc) | (uint32_t)0x1);
     WRITE_PERI_REG(RTC_GPIO_CONF, (READ_PERI_REG(RTC_GPIO_CONF) & (uint32_t)0xfffffffe) | (uint32_t)0x0);
     WRITE_PERI_REG(RTC_GPIO_ENABLE, (READ_PERI_REG(RTC_GPIO_ENABLE) & (uint32_t)0xfffffffe) | (uint32_t)0x1);
-	
+
     os_timer_setfn(&led_timer, (os_timer_func_t *)led_service_cb, NULL);
     os_timer_arm(&led_timer, 1000, 1);
 }
@@ -26,4 +26,3 @@ void user_init(void)
 {
     system_init_done_cb(app_init);
 }
-
